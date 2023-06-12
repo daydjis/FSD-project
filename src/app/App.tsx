4db479d165adbec67d873e8b599ed7f0f8b3b18e
mironiukia <mironiukia@63pokupki.ru>
2023-06-12 19:06:50 +0400

    delete spase in name

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React, {Suspense, useContext, useState} from 'react';
-import "./styles /index.scss";
+import "app/styles/index.scss";
 import {Link, Route, Routes} from "react-router-dom";
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/themeProviders";
